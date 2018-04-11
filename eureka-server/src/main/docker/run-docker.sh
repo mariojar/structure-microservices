a61f7bbcaad1e651c3a26369045ccfd413409375
Mario Jaramillo <marioja76@hotmail.it>
2018-04-11 16:36:01 +0200

    hola todos

--- eureka-server/src/main/docker/run-docker.sh
+++ eureka-server/src/main/docker/run-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-			docker run -d -p 8761:8761 \
+			docker run -d -p 8762:8762 \
 			--log-driver json-file \
 			--log-opt max-size=20m \
 			--log-opt max-file=25 \
-			--name eureka-server \
+			--name eureka-server-node2 \
 			microservices-study/eureka-server
